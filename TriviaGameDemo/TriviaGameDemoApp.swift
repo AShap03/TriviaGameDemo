3387aa6f2d052316937c355313956adcfe7b8f5e
PavelUshakov03 <p.ushakov.2003@gmail.com>
2021-12-29 21:51:22 +0300

    Initial Commit

---
+++ TriviaGameDemo/TriviaGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameDemoApp.swift
+//  TriviaGameDemo
+//
+//  Created by Pavel Ushakov on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
