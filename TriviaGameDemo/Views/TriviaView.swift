9866623e506deb39c91c916c6ce28b55742710a5
PavelUshakov03 <p.ushakov.2003@gmail.com>
2021-12-29 23:01:14 +0300

    Finished basic UI

---
+++ TriviaGameDemo/Views/TriviaView.swift
@@ -0,0 +1,20 @@
+//
+//  TriviaView.swift
+//  TriviaGameDemo
+//
+//  Created by Pavel Ushakov on 29.12.21.
+//
+
+import SwiftUI
+
+struct TriviaView: View {
+    var body: some View {
+        QuestionView()
+    }
+}
+
+struct TriviaView_Previews: PreviewProvider {
+    static var previews: some View {
+        TriviaView()
+    }
+}
